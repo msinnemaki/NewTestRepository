8fda122c2ba9bb374cea29a80605dcc6148fccc3
CVDavis104 <charell.v.davis@gmail.com>
2022-06-22 13:35:09 -0700

    Trying to commit

---
+++ TestRepository/src/main/java/Nonsense_Class.java
@@ -0,0 +1,7 @@
+public class Nonsense_Class {
+    public void nonsenseConstructor(String[] args){
+        int x;
+        for(x = 0;x > 10;x++){System.out.println("This is the number " + (x));}
+
+    /*'nonsenseConstructor' code block ending*/}
+    /*'Nonsense_CLass' code block ending*/}
